25dc8060973d3050058fcbf2e06d00027c5dd39d
roman hrytsiuk <rmngrck97@gmail.com>
2023-09-05 16:28:27 +0300

    added burger

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
     const pages = document.querySelectorAll(".page");
+    const checkbox = document.querySelector('#burger-checkbox')
+    const burgerIcon = document.querySelector('.burger__icon')
+    const burgerLines = document.querySelectorAll('.burger__line')
     let touchStartY = 0;
 
     // Function to scroll to the next page
     function scrollToNextPage() {
         const currentPage = document.querySelector(".page.active");
         const nextPage = currentPage.nextElementSibling;
+
+        if (nextPage === pages[2]) {
+            changeBurgerIcon(true)
+        } else {
+            changeBurgerIcon(false)
+        }
 
         if (nextPage) {
             currentPage.classList.remove("active");
@@ -18,6 +27,12 @@ document.addEventListener("DOMContentLoaded", function () {
     function scrollToPreviousPage() {
         const currentPage = document.querySelector(".page.active");
         const previousPage = currentPage.previousElementSibling;
+
+        if (previousPage === pages[2]) {
+            changeBurgerIcon(true)
+        } else {
+            changeBurgerIcon(false)
+        }
 
         if (previousPage) {
             currentPage.classList.remove("active");
@@ -54,6 +69,16 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
+    function changeBurgerIcon(isWhite) {
+        if (isWhite) {
+            burgerIcon.style.border = '2px solid #000'
+            burgerLines.forEach(line => line.style.backgroundColor = '#000')
+        } else {
+            burgerIcon.style.border = '2px solid #FFF'
+            burgerLines.forEach(line => line.style.backgroundColor = '#FFF')
+        }
+    }
+
     // Add event listeners
     document.addEventListener("touchstart", handleTouchStart, false);
     document.addEventListener("touchmove", handleTouchMove, { passive: false });
@@ -80,5 +105,27 @@ document.addEventListener("DOMContentLoaded", function () {
                 targetSection.classList.add("active")
             }
         });
+    });
+
+    checkbox.addEventListener('change', () => {
+        const currentPage = document.querySelector(".page.active");
+        if (checkbox.checked) {
+            burgerIcon.style.border = '2px solid #FFF'
+            burgerLines.forEach(line => line.style.backgroundColor = '#FFF')
+        } else if (currentPage === pages[2]) {
+            burgerIcon.style.border = '2px solid #000'
+            burgerLines.forEach(line => line.style.backgroundColor = '#000')
+        }
+    })
+});
+
+// Handle hide burger if link clicked
+
+const menuLinks = document.querySelectorAll('.burger__menu a');
+const burgerCheckbox = document.querySelector('#burger-checkbox');
+
+menuLinks.forEach(function(link) {
+    link.addEventListener('click', function() {
+        burgerCheckbox.checked = false;
     });
 });
